a261f300f88efb50edef3e72c020a64c57a06c83
Stefaan Avonds <Stefaan.Avonds@hotmail.com>
2017-05-02 12:53:20 +0200

    Bug fix Android

--- Hackaton/Hackaton.Droid/MainActivity.cs
+++ Hackaton/Hackaton.Droid/MainActivity.cs
@@ -38,13 +38,27 @@ namespace Hackaton.Droid
         {
             // We need to have at least the following permissions
             // From Android 7.0 we need to have Location-permission in order to scan and connect to a Bluetooth LE device ... for some reason ...
-            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Bluetooth) != Permission.Granted ||
+            if (Android.OS.Build.VERSION.SdkInt <= BuildVersionCodes.M)
+            {
+                if (CheckSelfPermission(Manifest.Permission.Bluetooth) != Permission.Granted ||
+                CheckSelfPermission(Manifest.Permission.BluetoothAdmin) != Permission.Granted ||
+                CheckSelfPermission(Manifest.Permission.BluetoothPrivileged) != Permission.Granted ||
+                CheckSelfPermission(Manifest.Permission.AccessCoarseLocation) != Permission.Granted ||
+                CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(Permissions, 0);
+                }
+            }
+            else
+            {
+                if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Bluetooth) != Permission.Granted ||
                 ActivityCompat.CheckSelfPermission(this, Manifest.Permission.BluetoothAdmin) != Permission.Granted ||
                 ActivityCompat.CheckSelfPermission(this, Manifest.Permission.BluetoothPrivileged) != Permission.Granted ||
                 ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Permission.Granted ||
                 ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Permission.Granted)
-            {
-                ActivityCompat.RequestPermissions(this, Permissions, 0);
+                {
+                    ActivityCompat.RequestPermissions(this, Permissions, 0);
+                }
             }
         }
     }
--- Hackaton/Hackaton/Constants.cs
+++ Hackaton/Hackaton/Constants.cs
@@ -12,6 +12,6 @@ namespace Hackaton
         /// The UUID for the Scratch Data service of a Bean-device.
         /// </summary>
         public static Guid BeanServiceScratchDataUuid = new Guid("a495ff20-c5b1-4b44-b512-1370f02d74de");
-        public static Guid BeanCharacteristicScratchDataAccelerometerUuid = new Guid("a495ff22-c5b1-4b44-b512-1370f02d74de");
+        public static Guid BeanCharacteristicScratchDataAccelerometerUuid = new Guid("a495ff21-c5b1-4b44-b512-1370f02d74de");
     }
 }
